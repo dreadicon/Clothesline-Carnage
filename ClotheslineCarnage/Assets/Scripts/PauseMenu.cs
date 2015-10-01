ea77147f4cbc3baf14e2287eb52ea14b6b9a06ad
Brad Petry <dreadicon@gmail.com>
2015-10-01 15:02:43 -0700

    Fixed project, added start of main menu
    -Forgot to save the scene/project before last commit...

---
+++ ClotheslineCarnage/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : Menu {
+
+
+
+	protected override void Awake () {
+		base.Awake ();
+		menuCanvas.enabled = false;
+		GetComponent<PauseMenu> ().enabled = false;
+	}
+
+	protected override void Update () {
+		base.Update ();
+	}
+
+	public override void Back () {
+		
+	}
+
+
+	public void ReturnToMainMenu () {
+		Application.LoadLevel ("MainMenu");
+	}
+	
+}
